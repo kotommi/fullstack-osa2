a197edecb1a386618f0a37c198b1642611caaeeb
kotommi <tommi.koivula@helsinki.fi>
2019-01-27 11:04:12 +0200

    puhelinluettelo

---
+++ puhlu/src/App.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+
+const Filter = ({ filter, handleFilter }) => {
+  return (
+    <div>
+      rajaa näytettäviä <input value={filter} onChange={handleFilter} />
+    </div>
+  );
+};
+
+const PersonForm = props => {
+  return (
+    <form onSubmit={props.addContact}>
+      <h3>lisää uusi</h3>
+      <div>
+        nimi: <input value={props.newName} onChange={props.handleName} />
+      </div>
+      <div>
+        numero: <input value={props.newNumber} onChange={props.handleNumber} />
+      </div>
+      <div>
+        <button type="submit">lisää</button>
+      </div>
+    </form>
+  );
+};
+
+const PersonList = ({ getContacts }) => {
+  return (
+    <div>
+      <h2>Numerot</h2>
+      <ul>{getContacts()}</ul>
+    </div>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arton Hella", number: "050-1234" },
+    { name: "M Luuk", number: "1234" },
+    { name: "T Koiv", number: "15556" }
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const personsToShow =
+    filter.length === 0
+      ? persons
+      : persons.filter(person =>
+          person.name.toLowerCase().includes(filter.trim().toLowerCase())
+        );
+
+  const addContact = event => {
+    event.preventDefault();
+    const contactObject = {
+      name: newName,
+      number: newNumber
+    };
+    if (persons.map(person => person.name).indexOf(newName) !== -1) {
+      window.alert(newName + " on jo luettelossa");
+      return;
+    }
+    setPersons(persons.concat(contactObject));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const getContacts = () => {
+    return personsToShow.map(person => (
+      <li key={person.name}>
+        {person.name} {person.number}
+      </li>
+    ));
+  };
+
+  const handleName = event => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumber = event => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilter = event => {
+    setFilter(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Puhelinluettelo</h2>
+      <Filter filter={filter} handleFilter={handleFilter} />
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleName={handleName}
+        handleNumber={handleNumber}
+        addContact={addContact}
+      />
+      <PersonList getContacts={getContacts} />
+    </div>
+  );
+};
+
+export default App;
